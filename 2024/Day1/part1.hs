5594b0075afd97e254ac31039586266838c76382
Michael Ulicny <michaelaulicny@gmail.com>
2024-12-01 15:04:18 -0500

    cleaner impl of part1

--- 2024/Day1/part1.hs
+++ 2024/Day1/part1.hs
@@ -1,17 +1,8 @@
-import Data.List (delete, minimum)
+import Data.List (sort)
 
 main :: IO ()
 main = do
     content <- readFile "input.txt"
     let (list1, list2) = unzip $ [(read x, read y) | [x, y] <- map words (lines content)]
-    let totalDist = getMins list1 list2
+    let totalDist = sum $ zipWith (\x y -> abs(x - y)) (sort list1) (sort list2)
     print totalDist
-
-getMins :: [Int] -> [Int] -> Int
-getMins [] [] = 0
-getMins list1 list2 = abs(min1 - min2) + getMins remain1 remain2
-    where
-        min1 = minimum list1
-        min2 = minimum list2
-        remain1 = delete min1 list1
-        remain2 = delete min2 list2
